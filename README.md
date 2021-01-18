d22d15452da9474c52f794894617a57636d5ffe3
puba.yu <puba.yu@kakaocorp.com>
2021-01-18 19:03:01 +0900

    feat : boilerplate 설정
    - sass와 기본적인 webpack, babel을 설정
    - 내용들 readme에 작성

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # webpack-babel-boilerplate
+
+webpack과 babel 설정이 되어 있는 boilerplate입니다.
+
+## 설정
+
+- sass, scss를 위한 "style-loader", "css-loader", "sass-loader" 설치
+- babel을 위하여 "babel-loader" 설치
+- polyfill을 위한 "core-js" 설치
+- babel을 구동하기 위하여 "@babel/cli", "@babel/core", "@babel/preset-env" 설치
+
+## 진입점 및 결과물 생성 폴더
+
+### 진입점
+
+`./public/js/main.js`
+
+### 결과물
+
+`public/dist`
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require("path");
+
+module.exports = {
+  mode: "development",
+  entry: "./public/js/main.js",
+  output: {
+    path: path.resolve(__dirname, "public/dist"),
+    filename: "bundle.js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
+};
